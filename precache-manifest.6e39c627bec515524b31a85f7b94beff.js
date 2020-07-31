3423ecd05fda253d1e8255902bdf4e8fbbf5dfeb
akrock0512 <akrock0512000@gmail.com>
2020-07-31 15:35:00 +0530

    Updates

---
+++ precache-manifest.6e39c627bec515524b31a85f7b94beff.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c506e7834d71919e4aa3e1792bca045d",
+    "url": "/KBL/index.html"
+  },
+  {
+    "revision": "466f4727c08c8b88352b",
+    "url": "/KBL/static/css/main.50dc5779.chunk.css"
+  },
+  {
+    "revision": "1366af97875551fdcc41",
+    "url": "/KBL/static/js/2.76a010cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/KBL/static/js/2.76a010cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "466f4727c08c8b88352b",
+    "url": "/KBL/static/js/main.b6967a8f.chunk.js"
+  },
+  {
+    "revision": "513e2183eea5baef7461",
+    "url": "/KBL/static/js/runtime-main.548fb756.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/KBL/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/KBL/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "66de507698338f6eec42a5cf44dc519b",
+    "url": "/KBL/static/media/book-menu.66de5076.svg"
+  },
+  {
+    "revision": "7ae93773851dc72adea8a13bb9dc457f",
+    "url": "/KBL/static/media/close.7ae93773.svg"
+  },
+  {
+    "revision": "9555c55e3a50976bbe3737aa404ad6d2",
+    "url": "/KBL/static/media/logo.9555c55e.svg"
+  }
+]);
